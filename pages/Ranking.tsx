ae43ec08e7747e0b151b91bc2fcc5ff3ad73f4ef
yanatori <yanatori333@gmail.com>
2022-07-11 21:22:36 +0900

    add:ranking

---
+++ pages/Ranking.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { css } from "@emotion/react";
+import Link from "next/link";
+
+export const Ranking = () => {
+  return (
+    <section>
+      <h2>Ranking</h2>
+    </section>
+  );
+};
